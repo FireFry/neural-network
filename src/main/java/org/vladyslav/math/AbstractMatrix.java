50a9a49800d754756b9702048234ac2bd875f8a9
FireFry <v.a.burakov@gmail.com>
2015-05-15 21:52:36 +0300

    Change matrix toString representation.

--- src/main/java/org/vladyslav/math/AbstractMatrix.java
+++ src/main/java/org/vladyslav/math/AbstractMatrix.java
@@ -1,26 +1,30 @@
 package org.vladyslav.math;
 
+import java.util.Formatter;
+
 public abstract class AbstractMatrix implements Matrix {
     @Override
     public String toString() {
         StringBuilder b = new StringBuilder();
-        b.append("Matrix[");
+        b.append("Matrix(");
         b.append(rows());
         b.append(":");
         b.append(cols());
-        b.append("]");
-        b.append('{');
+        b.append(") [");
         for (int row = 0; row < rows(); row++) {
-            b.append('{');
+            b.append('[');
             for (int col = 0; col < cols(); col++) {
-                b.append(get(row, col));
+                b.append(new Formatter().format("%.2f", get(row, col)));
                 if (col < cols() - 1) {
-                    b.append(", ");
+                    b.append(" ");
                 }
             }
-            b.append('}');
+            b.append("]");
+            if (row < rows() - 1) {
+                b.append(" ");
+            }
         }
-        b.append('}');
+        b.append(']');
         return b.toString();
     }
 }
