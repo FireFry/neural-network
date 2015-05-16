ef356f1385709e172061e89123b005b753cd6da2
FireFry <v.a.burakov@gmail.com>
2015-05-16 23:34:31 +0300

    Minor refactoring

--- src/main/java/org/vladyslav/nn/Main.java
+++ src/main/java/org/vladyslav/nn/Main.java
@@ -7,7 +7,7 @@ import static org.vladyslav.nn.Matrix.*;
 public class Main {
     private static final Random RANDOM = new Random(31415926);
 
-    private static final Matrix BINARY_INPUT = combineRows(row(0, 0),row(0, 1),row(1, 0),row(1, 1));
+    private static final Matrix BINARY_INPUT = combineRows(row(0, 0), row(0, 1), row(1, 0), row(1, 1));
 
     public static void main(String[] args) {
         Matrix x = BINARY_INPUT;
@@ -22,13 +22,16 @@ public class Main {
             Matrix a2 = a1.addBias().multiply(w1).sigmoid();
 
             Matrix d2 = y.minus(a2);
-            Matrix d1 = d2.multiply(w1.transpose()).removeBias().product(a1).product(of(a1.rows(), a1.cols(), 1.0).minus(a1));
+            Matrix d1 = d2.multiply(w1.transpose()).removeBias().product(a1).product(Matrix.of(a1.rows(), a1.cols(), 1.0).minus(a1));
 
             w1 = w1.plus(a1.addBias().transpose().multiply(d2).product(lambda));
             w0 = w0.plus(x.addBias().transpose().multiply(d1).product(lambda));
         }
 
-        System.out.println();
-        System.out.println(x.addBias().multiply(w0).sigmoid().addBias().multiply(w1).sigmoid());
+        Matrix output = x.addBias().multiply(w0).sigmoid().addBias().multiply(w1).sigmoid();
+        System.out.println(w0.transpose());
+        System.out.println(w1.transpose());
+        System.out.println(output);
+        System.out.println("Error: " + output.minus(y).product(output.minus(y)).average());
     }
 }
--- src/main/java/org/vladyslav/nn/Matrix.java
+++ src/main/java/org/vladyslav/nn/Matrix.java
@@ -187,4 +187,18 @@ public abstract class Matrix {
                 "a = " + a + ", " +
                 "b = " + b);
     }
+
+    public double average() {
+        return sum() / cols() / rows();
+    }
+
+    public double sum() {
+        double sum = 0;
+        for (int row = 0; row < rows(); row++) {
+            for (int col = 0; col < cols(); col++) {
+                sum += get(row, col);
+            }
+        }
+        return sum;
+    }
 }
